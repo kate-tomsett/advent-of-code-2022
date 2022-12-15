dac11d1ac9ef2da23ff6babc28809be60f1b3e41
kate-tomsett <48573599+kate-tomsett@users.noreply.github.com>
2022-12-15 16:57:01 +0000

    bit more progress on day 12 part 1 but still doesnt work

--- src/day12.py
+++ src/day12.py
@@ -1,12 +1,45 @@
+import string
+
+def get_visible(current, path):
+    visible = [[current[0]+1, current[1]], [current[0]-1, current[1]], [current[0], current[1]+1], [current[0], current[1]-1]]
+    
+    remove_list = []
+    
+    for coord in visible:
+        if coord[0] < 0 or coord[1] < 0 or coord[0] > num_rows-1 or coord[1] > num_cols-1:
+            remove_list.append(coord)
+        if coord in path:
+            remove_list.append(coord)
+    
+    visible = [i for i in visible if i not in remove_list]
+    
+    return visible
+    
+
+def check_movement(current, current_path, visible, grid, solutions):
+    current_value = grid[current[0]][current[1]]
+    current_path.append(current)
+    print('value', current_value, 'at', current[0], ',', current[1])
+    print('visible:', visible) 
+    for coord in visible:
+        coord_value = grid[coord[0]][coord[1]]
+        if alphabet.index(coord_value) < alphabet.index(current_value)+1:
+            solutions = check_movement(coord, current_path, get_visible(coord, current_path), grid, solutions)
+    if current == E:
+        print('found the end with path', current_path)
+        solutions.append(current_path)
+    return solutions
+
+
 fp = open('test-input.txt')
  
 rows = fp.read().split('\n')
 
+alphabet = list(string.ascii_letters)
+
 grid = []
-S_row = 0
-S_col = 0
-E_row = 0
-E_col = 0
+S = [0,0]
+E = [0,0]
 current = [0, 0]
 
 solutions = []
@@ -20,12 +53,20 @@ for row in range(0, num_rows):
     for col in range(0, num_cols):
         grid[row].append(rows[row][col])
         if rows[row][col] == 'S':
-            S_row = row
+            S = [row, col]
             current = [row, col]
-            S_col = col
+            grid[row][col] = 'a'
         elif rows[row][col] == 'E':
-            E_row = row
-            E_col = col
+            E = [row, col]
+            grid[row][col] = 'z'
 
-while !solutions_found:
-    visable =
+current_path = []
+
+while not solutions_found:
+    visible = get_visible(current, current_path)
+    solutions = check_movement(current, current_path, visible, grid, solutions)
+        
+    print(visible)
+    solutions_found = True
+   
+
